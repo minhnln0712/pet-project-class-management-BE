5e4d7129f5d51b54dfd53d1177146371ca0e4e39
minhnln0712 <minhnln0712@gmail.com>
2022-09-13 23:44:13 +0700

    first commit

---
+++ src/main/java/com/example/classmanagement/petprojectclassmanagement/PetProjectClassManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.classmanagement.petprojectclassmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectClassManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetProjectClassManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/classmanagement/petprojectclassmanagement/PetProjectClassManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classmanagement.petprojectclassmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectClassManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
